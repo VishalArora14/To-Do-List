d4f423ac892dd17b1c268b55f49e9ba7556c4856
VishalArora14 <poojaarora5082@gmail.com>
2023-03-06 11:17:53 +0530

    6 mar 2023

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,2 +1,2 @@
-export const BACKEND_BASE_API = "http://localhost:4000";
-// export const BASE_API = "";
+// export const BACKEND_BASE_API = "http://localhost:4000";
+export const BACKEND_BASE_API = "https://to-do-list-lemon-chi.vercel.app";
--- server.js
+++ server.js
@@ -69,3 +69,11 @@ if (port == null || port == "") {
 app.listen(port, function () {
   console.log(`Server has started successfully on Port ${port}`);
 });
+
+if (process.env.NODE_ENV == "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
